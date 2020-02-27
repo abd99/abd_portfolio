04c94d56f121e3be593674daca650fb4deaadd6b
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-02-27 18:27:08 +0530

    Add Navigation Buttons to small screen

---
+++ lib/components/navbutton.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NavButton extends StatelessWidget {
+  final text;
+  final onPressed;
+  final Color color;
+
+  const NavButton(
+      {Key key,
+      @required this.text,
+      @required this.onPressed,
+      this.color = Colors.orange})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return OutlineButton(
+      child: Text(text),
+      borderSide: BorderSide(
+        color: color,
+      ),
+      onPressed: onPressed,
+      highlightedBorderColor: color,
+    );
+  }
+}
--- lib/profile_page.dart
+++ lib/profile_page.dart
@@ -1,8 +1,52 @@
+import 'package:abd_portfolio/responsive_widget.dart';
 import 'package:flutter/material.dart';
 
+import 'components/navbutton.dart';
+
 class ProfilePage extends StatelessWidget {
+  List<Widget> navButtons() => [
+        NavButton(
+          text: "About",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "Education",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "Skills",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "Projects",
+          onPressed: () {},
+        ),
+        NavButton(
+          text: "Contact",
+          onPressed: () {},
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                  padding: EdgeInsets.all(16.0),
+                  children: navButtons(),
+                ),
+              )
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            duration: Duration(seconds: 1),
+            child: ResponsiveWidget(largeScreen: Column()),
+          ),
+        ),
+      ),
+    );
   }
 }
