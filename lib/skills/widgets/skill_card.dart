6365e9079ce921d3bfccb2756358d4771e0655e6
“abd99” <abdullahzakir99@gmail.com>
2021-03-25 16:29:23 +0530

    Fix spacing between skill icon and text

--- lib/skills/widgets/skill_card.dart
+++ lib/skills/widgets/skill_card.dart
@@ -15,24 +15,30 @@ class SkillCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var isSmallScreen = ResponsiveWidget.isSmallScreen(context);
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          FaIcon(
-            icon,
-            size: ResponsiveWidget.isLargeScreen(context) ? 80.0 : 60.0,
-          ),
-          const SizedBox(
-            height: 16.0,
-          ),
-          Text(
-            label,
-            style: kLabelTextStyle,
-          )
-        ],
+      child: Container(
+        height: isSmallScreen ? 105.0 : 125.0,
+        width: isSmallScreen ? 105.0 : null,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            FaIcon(
+              icon,
+              size: ResponsiveWidget.isLargeScreen(context) ? 80.0 : 60.0,
+            ),
+            const SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              label,
+              style: kLabelTextStyle,
+              overflow: TextOverflow.ellipsis,
+            )
+          ],
+        ),
       ),
     );
   }
