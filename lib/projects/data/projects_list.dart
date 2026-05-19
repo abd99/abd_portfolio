c372f0d9d9394f372fee45b6bb47eab6268d83f8
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2026-05-19 19:48:01 +0400

    Update projects

--- lib/projects/data/projects_list.dart
+++ lib/projects/data/projects_list.dart
@@ -4,19 +4,30 @@ import '../models/project.dart';
 
 const List<Project> projectsList = [
   Project(
+    title: 'YesPls Marketplace',
+    subTitle: 'Shopping App for the Lao Region',
+    description: [
+      'Lead contributor in rebuilding the #1 shopping app in the Lao region.',
+      '- 30,000+ downloads on the app stores.',
+      '- Re-architected the app for scalability and performance.',
+    ],
+    year: 'Oct 2021 - May 2022',
+    techStack: 'Flutter, Firebase, REST APIs',
+  ),
+  Project(
     title: 'MSBTE Study Guide',
     subTitle: 'Study Resource App',
     description: [
-      'Provides study resources for G-Scheme MSBTE Students such as: ',
-      '- Cirriculum',
-      '- Questions Papers',
-      '- Solved Modal Answer Papers',
+      'Developed a study resource app that offers exam preparation resources for G-Scheme MSBTE students such as:',
+      '- Curriculum',
+      '- Question Papers',
+      '- Solved Model Answer Papers',
     ],
-    year: '2017 - 2019',
-    techStack: 'Native Android(Java), Firebase Cloud Storage',
+    year: 'Jan 2018 - Mar 2018',
+    techStack: 'Native Android (Java), Firebase Cloud Storage',
     textWithLinks: TextWithLinks(
       textArray: [
-        CustomURL(text: '- More than 10K downloads on the '),
+        CustomURL(text: '- 20,000+ downloads on the '),
         CustomURL(
           text: 'Google Play Store',
           url:
@@ -27,23 +38,23 @@ const List<Project> projectsList = [
   ),
   Project(
     title: 'Med-Dose',
-    subTitle: ' Medicine Dosage Reminder',
+    subTitle: 'Medicine Dosage Reminder',
     description: [
       'The app:',
       '- Reminds the user to take medicine based upon the set daily dosage times.',
       '- Rings an optional alarm on the dosage time.',
       '- Keeps a track of the user\'s dosage.',
     ],
-    year: '2018 - 2018',
-    techStack: 'Native Android(Java), SQLite',
+    year: '2018',
+    techStack: 'Native Android (Java), SQLite',
   ),
   Project(
     title: 'MyKAAR',
-    subTitle: 'IoT based Car Immobilizer System',
+    subTitle: 'IoT-based Car Immobilizer System',
     description: [
-      'A system that provides the user to control immolizing features for his/her car such as cut-off the ignition, locate the car, etc. using an app.',
+      'A system that lets the user control immobilizing features for their car such as cutting-off the ignition, locating the car, etc. through a companion app.',
     ],
     year: '2017 - 2018',
-    techStack: 'Native Android(Java), Arduino, Java Servlet',
+    techStack: 'Native Android (Java), Arduino, Java Servlet',
   ),
 ];
